770668b655bf3d0e2fafd4043f917139affccce2
LeonCzerwinski <poriusz@wp.pl>
2020-07-17 16:08:59 +0200

    Added home and artist/:name routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,31 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import ArtistView from '../views/Artist.vue';
+import HomeView from '../views/Home.vue';
+import artists from '../artists';
+
+const getShuffledArtists = () => artists.sort(() => Math.random() - 0.5);
+
+const getArtist = name => artists.find(i => i.text.toLowerCase() == name);
 
 Vue.use(VueRouter)
 
   const routes = [
- ]
+    { 
+      path: '/artist/:name', 
+      name: 'artist',
+      component: ArtistView,
+      props: (route) => ({ artist: getArtist(route.params.name) })
+    },
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      props:{
+        artists: getShuffledArtists()
+      }
+    }
+  ]
 
 const router = new VueRouter({
   mode: 'history',
