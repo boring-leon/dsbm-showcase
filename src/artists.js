eca2d71c2f77e3fca1a369e5b5bcab257cac4558
LeonCzerwinski <poriusz@wp.pl>
2020-07-17 19:30:06 +0200

    Added info about some of the artists

--- src/artists.js
+++ src/artists.js
@@ -2,54 +2,68 @@ const artists = [
     {
         image: "austere.jpg",
         text: "Austere",
-        path: "Austere - To Fade With The Dusk.mp3"
+        path: "Austere - To Fade With The Dusk.mp3",
+        about: `Formed in 2005 in Australia by Mitchell <i>„Desolate”</i> Keepin, joined in 2007 by Tim <i>„Sorrow"</i> Yatras, Austerte features a very dense, melancholic atmosphere, that's so easy to get immersed into.`
     },
     {
         image: "drowned.jpg",
         text: "Drowned",
-        path: "Drowned - 72 Hours Awake Thinking About Suicide.mp3"
+        path: "Drowned - 72 Hours Awake Thinking About Suicide.mp3",
+        about: `Drowned is a duo from Brazil. Their music puts heavy emphasis on feelings of hatred and anguish.`
     },
     {
         image: "hopeless.jpg",
         text: "Hopeless",
-        path: "Hopeless - My Razor My Crown.mp3"
+        path: "Hopeless - My Razor My Crown.mp3",
+        about: `Hopeless is a spanish one man band founded in 2008 by S.<br/> He's style seems to be influenced by atmospheric black and doom metal.`
     },
     {
         image: "life_is_pain.jpg",
         text: "Life is pain",
         path: "Life Is Pain - Oppressive Nights In Mental Asylum.mp3",
-        opacity: "25%"
+        opacity: "25%",
+        about: `Life Is Pain formed in 2006 by <i>Trist</i> and <i>Kim Carlsson</i>, putting out a demo CD titled <i>Bloody Melancholy</i>. The project ended in the same year, since Kim wanted to focus more on <i>Lifelover</i>.
+        `
     },
     {
         image: "my_useless_life.jpg",
         text: "My useless life",
         path: "My Useless Life - A Heart Full of Sorrow.mp3",
-        opacity: "45%"
+        opacity: "45%",
+        about: `Founded in September 2011 by <i>F</i>, joined by <i>S</i> in November 2011. The overdrive effect and echoing vocals, present on most of their tracks, can really make you feel like your lost in a maze of auditory fog.`
     },
     {
         image: "no_point_in_living.jpg",
         text: "No point in living",
-        path: "No Point In Living - Urban Melancholy.mp3"
+        path: "No Point In Living - Urban Melancholy.mp3",
+        about: ``
     },
     {
         image: "sacrimoon.jpg",
         text: "Sacrimoon",
-        path: "Sacrimoon - Solitude and Coldness.mp3"
+        path: "Sacrimoon - Solitude and Coldness.mp3",
+        about: ``
     },
     {
         image: "through_the_pain.jpg",
         text: "Through the pain",
-        path: "Through the Pain - Time Heals Nothing.mp3"
+        path: "Through the Pain - Time Heals Nothing.mp3",
+        about: ``
     },
     {
         image: "trist.jpg",
         text: "Trist",
-        path: "Trist - Těžkost bytí.mp3"
+        path: "Trist - Těžkost bytí.mp3",
+        about: `Jan Šincl: "<i>I recorded music when I was set free from  mental asylum. It was recorded in few days, I had inspiration. I think I won’t make more music in the future. Now my life is alcohol and I think it will destroy me.</i>"<br/><br/>
+        The word "trist" translates to „depressing”, „dreary” or „sad” in many languages.
+        He's music is raw, hopless, trance-inducing. Immense.
+        `
     },
     {
         image: "wintercult.jpg",
         text: "Wintercult",
-        path: "Wintercult - Frozen in Melancholy.mp3"
+        path: "Wintercult - Frozen in Melancholy.mp3",
+        about: ``
     },
     {
         image: "xasthur.jpg",
@@ -60,7 +74,8 @@ const artists = [
     {
         image: "xathrites.jpg",
         text: "Xathrites",
-        path: "Xathrites - My Last Day Story"
+        path: "Xathrites - My Last Day Story.mp3",
+        about: ``
     }
 
 ];
