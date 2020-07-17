f782bf68c878527270d58c8b0b9adf3410d5363c
LeonCzerwinski <poriusz@wp.pl>
2020-07-17 16:09:49 +0200

    Created HomeView and ArtistView

---
+++ src/views/Artist.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="text-center">
+    <h1>{{ artist.text }}</h1>
+    <ArtistInfo :artist="artist" />
+    <SoundPlayer :artist="artist" />
+  </div>
+</template>
+
+<script>
+import SoundPlayer from "../components/SoundPlayer";
+import ArtistInfo from "../components/ArtistInfo";
+
+export default {
+  name: "ArtistView",
+  components: { SoundPlayer, ArtistInfo },
+  props:{
+    artist:{
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.text-center{
+  text-align: center;
+  margin-top: 5px;
+  padding: 10px;
+  position: relative;
+  height: 100%;
+}
+p{
+  font-size:2.5em
+}
+
+h1{
+  font-size: 5.5rem;
+  font-weight: 300;
+  line-height: 1.2;
+}
+
+i{
+  font-size: 2.2rem;
+}
+
+@media only screen and (max-width: 768px) {
+  p {
+    font-size: 1.5em;
+  }
+  i{
+    font-size: 1.3rem;
+  }
+  h1{
+    font-size: 2rem;
+  }
+  button{
+    margin-left:-140px;
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="grid-container">
+    <router-link
+      v-for="(artist, key) in artists"
+      :key="key"
+      :to="{name: 'artist', params: {'name' : artist.text.toLowerCase() }}"
+    >
+      <Artist :key="key" :artist="artist" />
+    </router-link>
+  </div>
+</template>
+
+<script>
+import Artist from "../components/Artist";
+
+export default {
+  name: "HomeView",
+  components: { Artist },
+  props:{
+    artists:{
+      type: Array,
+      required: true
+    }
+  },
+};
+</script>
