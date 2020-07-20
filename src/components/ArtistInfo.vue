fd44f3b7673c17f0c5ea3a5ce32f4ff3ae9df48b
LeonCzerwinski <poriusz@wp.pl>
2020-07-20 15:42:21 +0200

    Removing Typewriter cursor from the DOM after the typing is done

--- src/components/ArtistInfo.vue
+++ src/components/ArtistInfo.vue
@@ -8,16 +8,22 @@ import Typewriter from "typewriter-effect/dist/core";
 export default {
   name: "ArtistInfo",
   props: {
-    artist:{
+    artist: {
       type: Object,
       required: true
     }
   },
-  mounted(){
-    new Typewriter(`#${this.artistID}_info`).typeString(this.artist.about).start();
+  mounted() {
+    const writer = new Typewriter(`#${this.artistID}_info`);
+    writer
+      .typeString(this.artist.about)
+      .start()
+      .callFunction(() => {
+        document.querySelector(".Typewriter__cursor").remove();
+      });
   },
-  computed:{
-    artistID(){
+  computed: {
+    artistID() {
       const s = this.artist.image;
       return s.substring(0, s.length - 4);
     }
