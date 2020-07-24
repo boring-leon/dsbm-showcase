deff069a80baaf730c48a10bc0bad241e5de592f
LeonCzerwinski <poriusz@wp.pl>
2020-07-24 14:13:02 +0200

    Typing effect only fires off on the first visit on a given artist subpage

--- src/components/ArtistInfo.vue
+++ src/components/ArtistInfo.vue
@@ -13,20 +13,39 @@ export default {
       required: true
     }
   },
-  mounted() {
-    const writer = new Typewriter(`#${this.artistID}_info`);
-    writer
-      .typeString(this.artist.about)
-      .start()
-      .callFunction(() => {
-        const cursor = document.querySelector(".Typewriter__cursor");
-        cursor ? cursor.remove() : () => {};
-      });
+  beforeDestroy() {
+    this.$store.commit("addVisitedArtistRoute", this.artistID);
   },
+
+  mounted() {
+    const root = document.querySelector(`#${this.artistID}_info`);
+
+    if (this.hasArtistBeenAlreadyVisited) {
+      root.innerHTML = this.artist.about;
+    } else {
+      this.startWriting(root);
+    }
+  },
+  methods: {
+    startWriting(root) {
+      const writer = new Typewriter(root);
+      writer
+        .typeString(this.artist.about)
+        .start()
+        .callFunction(() => {
+          const cursor = document.querySelector(".Typewriter__cursor");
+          cursor ? cursor.remove() : () => {};
+        });
+    }
+  },
+
   computed: {
     artistID() {
       const s = this.artist.image;
       return s.substring(0, s.length - 4);
+    },
+    hasArtistBeenAlreadyVisited() {
+      return this.$store.state.visitedArtists.includes(this.artistID);
     }
   }
 };
