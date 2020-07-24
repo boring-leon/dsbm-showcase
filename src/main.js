6e395f4b89e30c4fd67d8a014724ef72c7c6319c
LeonCzerwinski <poriusz@wp.pl>
2020-07-24 14:12:43 +0200

    Added Vuex store

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import store from './store';
 import './assets/scss/app.scss';
+
 import VueScrollReveal from 'vue-scroll-reveal';
  
 Vue.use(VueScrollReveal);
@@ -19,6 +20,7 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App),
 }).$mount('#app');
 
