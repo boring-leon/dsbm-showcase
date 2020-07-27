e8aed2f7a2b1bf5e983dc1e41f60eda7ce687d08
LeonCzerwinski <poriusz@wp.pl>
2020-07-27 13:57:59 +0200

    Created Modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="modal-mask" v-once>
+    <div class="modal-wrapper">
+      <div class="modal-container">
+        <div class="modal-body">
+        If you wish to skip the typing effect, <b>double click</b> on the text that's being typed
+        </div>
+
+        <div class="modal-footer">
+            <button class="modal-button" @click="$emit('close')">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+};
+</script>
